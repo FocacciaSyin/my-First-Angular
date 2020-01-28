9600e27720b91bcaad105b5fb291d244e193d915
Your Name <you@example.com>
2020-01-29 00:45:20 +0800

    update header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,10 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-
-<<<<<<< HEAD
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GuoRungModule } from './guo-rung/guo-rung.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-=======
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { GuoRungModule } from "./guo-rung/guo-rung.module";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
->>>>>>> 0b5678bd3abad7a9b42b72d8a7f796d781e951fd
+
 
 @NgModule({
   declarations: [AppComponent],
@@ -25,4 +17,4 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
